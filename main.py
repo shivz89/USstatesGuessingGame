2db6b9870fb7711a38925e8e28d9ffe55015ff39
Shivam Mishra <shivz89@gmail.com>
2022-12-19 00:24:41 +0530

    adding list comptehensions

--- main.py
+++ main.py
@@ -13,11 +13,9 @@ while len(guessed_states) <= 50:
     new_title = str(len(guessed_states)) + "/50 states guessed correctly"
     answer_state = screen.textinput(
         prompt="Guess the State", title=new_title).title()
+
     if answer_state == "Exit":
-        missing_states = []
-        for state in state_list:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in state_list if state not in guessed_states]
         missing_states_dict = {
             "Missing States": missing_states
         }
